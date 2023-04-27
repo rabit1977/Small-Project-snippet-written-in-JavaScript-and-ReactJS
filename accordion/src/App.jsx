7edb26abc7475e55edc18daaa1af0eb4cee8f1e0
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-04-27 23:54:51 +0200

    create Accordion component on ReactJS

---
+++ accordion/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './App.css';
+
+const items = [
+  {
+    title: 'Section 1',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    title: 'Section 2',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    title: 'Section 3',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+];
+
+function Accordion() {
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const handleClick = (index) => {
+    setActiveIndex(index === activeIndex ? -1 : index);
+  };
+  return (
+    <div>
+      {items.map((item, index) => (
+        <div key={item.title}>
+          <button onClick={() => handleClick(index)}>{item.title} </button>
+          {index === activeIndex && <p>{item.content}</p>}
+        </div>
+      ))}
+    </div>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+}
+
+export default App;
